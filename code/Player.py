52cb7cd109955e68caf7cd092fc36f5f0d4e8497
Horthega <horthega2@gmail.com>
2025-11-10 16:39:01 -0300

    Creating players and enemies moving players and enemies

--- code/Player.py
+++ code/Player.py
@@ -1,11 +1,24 @@
-from Entity import Entity
+import pygame
+
+from code.Const import ENTITY_SPEED, WIN_HEIGHT, PLAYER_KEY_UP, PLAYER_KEY_DOWN, PLAYER_KEY_LEFT, PLAYER_KEY_RIGHT
+from code.entity import Entity
+
 
 class Player(Entity):
-    def __init__(self):
-        pass
+    def __init__(self, nome: str, position: tuple):
+        super().__init__(nome, position)
 
     def update(self):
         pass
 
     def move(self, ):
+        preesed_keys = pygame.key.get_pressed()
+        if preesed_keys[PLAYER_KEY_UP[self.name]] and self.rect.top > 0:
+            self.rect.centery -= ENTITY_SPEED[self.name]
+        if preesed_keys[PLAYER_KEY_DOWN[self.name]] and self.rect.bottom < WIN_HEIGHT:
+            self.rect.centery += ENTITY_SPEED[self.name]
+        if preesed_keys[PLAYER_KEY_LEFT[self.name]] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        if preesed_keys[PLAYER_KEY_RIGHT[self.name]] and self.rect.right > 0:
+            self.rect.centerx += ENTITY_SPEED[self.name]
         pass
--- code/enemy.py
+++ code/enemy.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from turtle import position
 
-class Enemy:
-    def __init__(self):
-        pass
+from code.Const import ENTITY_SPEED, WIN_WIDTH
+from code.entity import Entity
+
+
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
 
     def move(self, ):
-        pass
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.rect.right <= 0:
+            self.rect.left = WIN_WIDTH
