e854ce45516b6fa375c4f428169878580ff41726
Horthega <horthega2@gmail.com>
2025-11-14 16:40:52 -0300

    Hit box collisions HUD Score

--- code/Const.py
+++ code/Const.py
@@ -1,9 +1,12 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_YELLOW = (255, 255, 128)
-COLOR_WHITE = (255, 255, 255)
+C_ORANGE = (255, 128, 0)
+C_YELLOW = (255, 255, 128)
+C_WHITE = (255, 255, 255)
+C_GREEN = (0, 100, 0)
+C_CYAN = (0, 255, 255)
+
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -16,9 +19,9 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
-    'Player1Shot': 1,
+    'Player1Shot': 10,
     'Player2': 3,
-    'Player2Shot': 3,
+    'Player2Shot': 10,
     'Enemy1': 2,
     'Enemy1Shot': 5,
     'Enemy2': 1,
@@ -45,11 +48,48 @@ ENTITY_HEALTH = {
 }
 
 ENTITY_SHOT_DELAY = {
-    'Player1': 20,
-    'Player2': 15,
+    'Player1': 5,
+    'Player2': 5,
     'Enemy1': 100,
     'Enemy2': 200,
 }
+
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 0,
+    'Player1Shot': 0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
+
 
 # M
 MENU_OPTION = ('NEW GAME 1P',
